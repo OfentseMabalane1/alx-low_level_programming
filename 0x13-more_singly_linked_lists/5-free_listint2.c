fdd1234d35eba56383e488b282b3aa15728b139d
OfentseMabalane1 <keitumetse.mabalane123@gmail.com>
2023-04-04 08:29:21 -0700

    5-free_listint2.c

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - free linked list
+ * @head: free head of the list
+ * Return: void
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *next;
+
+	if (!head)
+		return;
+
+	while (*head)
+	{
+		next = (*head)->next;
+		free(*head);
+		*head = next;
+	}
+}
