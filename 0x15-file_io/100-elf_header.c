8b99ad9179598336e66d3c77e52258d8d74f5564
OfentseMabalane1 <keitumetse.mabalane123@gmail.com>
2023-04-11 05:46:33 -0700

    100-elf_header.c

---
+++ 0x15-file_io/100-elf_header.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - program entry point
+ * argc: argument counter
+ * @argv: argument vector
+ * Return: 0 always
+ */
+
+int main(int argc, char **argv)
+{
+	(void) argc;
+	(void) argv;
+	return (0);
+}
